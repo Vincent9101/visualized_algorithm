2795782cfbdba54b26b49055897e7e5a9c89765d
Vincent <vincent9101@qq.com>
2018-09-17 23:33:49 +0800

    upodate on wingit

---
+++ src/com/github/com/vincent9101/visualized_algorithm/course_3/montecarlo/MonteCarloAlgoFrame.java
@@ -0,0 +1,15 @@
+package com.github.com.vincent9101.visualized_algorithm.course_3.montecarlo;
+
+public class MonteCarloAlgoFrame extends com.github.com.vincent9101.visualized_algorithm.frame_view.AlgoFrame {
+
+    private Circle circle
+    public MonteCarloAlgoFrame(String title, int canvasHeight, int canvasWidth) {
+        super(title, canvasHeight, canvasWidth);
+    }
+
+
+    @Override
+    public void render(Object[] data) {
+
+    }
+}
